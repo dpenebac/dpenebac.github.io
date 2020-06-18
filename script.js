0b0ecb9ed73e9cd4e7460e0cc8108861236e980e
dpenebac <60830278+dpenebac@users.noreply.github.com>
2020-06-17 22:38:04 -1000

    asdf

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function show()
-{
-    document.getElementById
+function myFunction() {
+    alert("YA MOM")
 }
 
+function enterData() {
+    var name;
+    name = document.getElementById("uname").value;
+    if (name + "file.html"){
+        window.open(name+"file.html");
+    }
+    else {
+        var blank = name;
+        saveAs(blank, name + "file.txt");
+    }
+}
 //dpenebac.gihub.io
