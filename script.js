5d032be93ec42b525026bb98e98498146f1bcdfc
dpenebac <60830278+dpenebac@users.noreply.github.com>
2020-06-18 15:51:30 -1000

    added sections and divs

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-function myFunction() {
-    alert("YA MOM")
-}
 
-function enterData() {
-    var name;
-    name = document.getElementById("uname").value;
-    if (name + "file.html"){
-        window.open(name+"file.html");
-    }
-    else {
-        var blank = name;
-        saveAs(blank, name + "file.txt");
-    }
-}
 //dpenebac.gihub.io
